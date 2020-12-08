90f79edaf903db611cc191c8323b6f66472e0363
Paul Brighton <brighton.paul@yahoo.com>
2020-12-08 18:00:16 +0000

    Added page for contact forms plus updated animation page

--- page-contact-forms.php
+++ page-contact-forms.php
@@ -1,3 +1,22 @@
 <?php get_header(); ?>
-<p>Contact Forms</p>
+<!-- Multi column layout with minimal styling -->
+<div class="c-contact-form">
+  <div class="o-container">
+    <h1>Contact Forms</h1>
+    <h2>Contact Form 1</h2>
+    <p class="c-contact-form-subtitle">Three column for name, email and subject.</p>
+    <?php echo do_shortcode('[contact-form-7 id="113" title="Contact form 1"]') ?>
+  </div>
+</div>
+<!-- Single column layout with minimal styling -->
+<div class="c-contact-form--full-width" id="contact-form">
+  <div class="o-container">
+    <h2>Contact Form 2</h2>
+    <p class="c-contact-form--full-width-subtitle">Equal width contact form.</p>
+    <div class="c-contact-form--full-width__container">
+      <?php echo do_shortcode('[contact-form-7 id="114" title="Contact Form 2"]'); ?>
+    </div>
+  </div>
+</div>
+
 <?php get_footer(); ?>
